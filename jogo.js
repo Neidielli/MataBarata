6fc233953d1a767ee395ebdf9f6c5508c60192a4
Neidielli <100215137+Neidielli@users.noreply.github.com>
2023-01-01 22:50:51 -0300

    Criando posições randomicas

--- jogo.js
+++ jogo.js
@@ -11,3 +11,22 @@ function ajustaPalco() {
 
 ajustaPalco()
 
+function posicaoRandom() {
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
+
+    // ajusta a probabilidade de posições negativas
+    posicaoX = posicaoX < 0 ? 0 : posicaoX 
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    // Criar o elementos html
+    var barata = document.createElement('img')
+    barata.src = 'imagens/Barata.png'
+    barata.className = 'barata1'
+    barata.style.left = posicaoX + 'px'
+    barata.style.top = posicaoY + 'px'
+    barata.style.position = 'absolute'
+
+    document.body.appendChild(barata)
+}
+
