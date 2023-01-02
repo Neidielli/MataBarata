f50c7c58f8d134d7f837d2778bf26a9b292e177f
Neidielli <100215137+Neidielli@users.noreply.github.com>
2023-01-01 22:04:24 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# MataBarata
+ 
---
+++ jogo.js
@@ -0,0 +1,13 @@
+ 
+var altura = 0
+var largura = 0 
+
+function ajustaPalco() {     
+    altura = window.innerHeight
+    largura = window.innerWidth
+
+    console.log(altura, largura)
+}
+
+ajustaPalco()
+
